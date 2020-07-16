ed153f398f557275b04767387f3d91523ff762ba
Joshua Argote <argoteme@usc.edu>
2020-07-15 18:28:28 -0700

    Added code to process and render backbuffer data to window

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,23 @@
 #include <windows.h>
 
+/* Main Macros */
+#define LOCAL_PERSIST static
+#define GLOBAL static
+#define SINGLE_DEFINE static
+#define INTERNAL_FUNCTION static
+
+/* Globals */
+// TODO: Global for now
+GLOBAL bool Running;
+GLOBAL BITMAPINFO BitmapInfo;
+GLOBAL void* BitmapBuffer;
+GLOBAL HBITMAP BitmapHandle;
+GLOBAL HDC BitmapDeviceContext;
+
 /* Function Prototypes */
 LRESULT __stdcall MainWindowCallBack(HWND hwnd, UINT uMsg, WPARAM uwParam, LPARAM lParam);
+INTERNAL_FUNCTION void Win32ResizeDIBSection(int width, int height);
+INTERNAL_FUNCTION void Win32UpdateWindow(HDC DeviceContext, int x, int y, int width, int height);    
 
 /*
 * Entry point for WINAPI Application.
@@ -84,16 +100,24 @@ LRESULT __stdcall MainWindowCallBack(HWND hwnd, UINT uMsg, WPARAM uwParam, LPARA
     {
         case WM_SIZE:
         {
+            RECT ClientRect;
+            GetClientRect(hwnd, &ClientRect);
+            int width = ClientRect.right - ClientRect.left;
+            int height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(width, height);
             OutputDebugString("WM_SIZE\n");
             break;
         }
         case WM_DESTROY:
         {
+            // TODO: Handle this with an error to the user? Recreate window?
             OutputDebugString("WM_DESTROY\n");
             break;
         }
         case WM_CLOSE:
         {
+            // TODO: Handle this with a message to the user?
+            PostQuitMessage(0);
             OutputDebugString("WM_CLOSE\n");
             break;
         }
@@ -110,6 +134,7 @@ LRESULT __stdcall MainWindowCallBack(HWND hwnd, UINT uMsg, WPARAM uwParam, LPARA
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
             int X = Paint.rcPaint.left;
             int Y = Paint.rcPaint.top;
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
             PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
             EndPaint(hwnd, &Paint);
             break;
@@ -124,4 +149,41 @@ LRESULT __stdcall MainWindowCallBack(HWND hwnd, UINT uMsg, WPARAM uwParam, LPARA
     }
 
     return Result;
+}
+
+/*
+* Resizes device independent bitmap
+*/
+INTERNAL_FUNCTION void Win32ResizeDIBSection(int width, int height)
+{
+    // TODO: Bulletproot this. Maybe do not free first, free after,
+    // then free first if that fails
+    // TODO: Free our DIBSection
+    if (BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    else
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = width;
+    BitmapInfo.bmiHeader.biHeight = height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    BitmapInfo.bmiHeader.biSizeImage = 0;
+    BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
+    BitmapInfo.bmiHeader.biYPelsPerMeter = 0;
+    BitmapInfo.bmiHeader.biClrUsed = 0;
+    BitmapInfo.bmiHeader.biClrImportant = 0;
+
+    BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapBuffer, NULL, NULL);
+}
+
+INTERNAL_FUNCTION void Win32UpdateWindow(HDC DeviceContext, int x, int y, int width, int height)    
+{
+    // StretchDIBits(DeviceContext, )
 }
